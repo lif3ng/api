96f1573cc025d52aa6bf0160e57767207d246097
lif3ng <lif3ng@icloud.com>
2021-08-24 14:16:21 +0800

    update and delete note

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -3,5 +3,26 @@ module.exports = {
     await models.Note.create({
       content: args.content,
       author: 'a author str'
-    })
+    }),
+  deleteNote: async (parent, { id }, { models }) => {
+    try {
+      await models.Note.findOneAndRemove({ _id: id });
+      return true;
+    } catch (err) {
+      // MongooseError [CastError]: Cast to ObjectId failed for value "xxxxxxxx" at path "_id" for model "Note"
+      console.log(err);
+      return false;
+    }
+  },
+  updateNote: async (parent, { id, content }, { models }) => {
+    return await models.Note.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          content
+        }
+      },
+      { new: true }
+    );
+  }
 };
